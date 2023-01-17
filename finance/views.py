a66b583c44ea8df441d8e848b421e97e3e321309
Tahseen Rahman <tahseen.rahman0996@gmail.com>
2023-01-17 22:47:06 +0530

    #6 (#10)
    * feat: :card_file_box: Kitchen Modles
    
    * Migrations & Refactoring
    
    * Finance App
    
    * Finance Sale Models
    
    * Requriements File Added
    
    * DB schema changes
    
    * SignUp Bug Fix & refactoring
    
    * Member authorization mixin
    
    * KOT creation
    
    * Refactoring
    
    * KOT GET API

--- finance/views.py
+++ finance/views.py
@@ -1,9 +1,10 @@
-from django.http import JsonResponse, HttpResponseBadRequest, HttpResponseForbidden
+from django.http import JsonResponse, HttpResponseBadRequest, HttpResponseForbidden, HttpResponseNotFound
 
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 
 from authentication.mixins import MemberAccessMixin
+from finance.models.sale import KOT
 
 from .serializers import KOTSerializer
 from .services.kot_service import KOTService
@@ -11,6 +12,22 @@ from .services.kot_service import KOTService
 
 class KOTView(APIView, MemberAccessMixin):
     permission_classes = (IsAuthenticated,)
+
+    def get(self, request, restaurant_id: str):
+        user = request.user
+        is_valid_member = self.is_authorized_for_restaurant(user, restaurant_id)
+        if not is_valid_member:
+            return HttpResponseForbidden("Invalid Member for the restaurant")
+
+        kot_id = request.query_params.get("id")
+
+        if not kot_id:
+            return HttpResponseBadRequest("kot_id param is missing")
+        try:
+            kot = KOT.objects.get(id=kot_id, restaurant_id=restaurant_id)
+        except KOT.DoesNotExist:
+            return HttpResponseNotFound("KOT does not exist")
+        return JsonResponse({"data": KOTSerializer(kot).data})
 
     def post(self, request, restaurant_id: str):
         """
