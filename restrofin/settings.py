c284b7740230a46166ff79d4b93522507e26728b
fullstacksuiit <rahmantanshit@gmail.com>
2023-01-20 17:08:11 +0530

    CORS

--- restrofin/settings.py
+++ restrofin/settings.py
@@ -30,9 +30,11 @@ INSTALLED_APPS = [
     'authentication',
     'kitchen',
     'finance',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -41,6 +43,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'restrofin.urls'
 
