d9686a75f7d47fc2c2114a873b8383e7a70e9cf5
Tahseen Rahman <tahseen.rahman0996@gmail.com>
2023-04-15 12:07:38 +0530

    Admin Panel Customization

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from authentication.models import Member, Restaurant
 
-# Register your models here.
-admin.site.register([Member, Restaurant])
+@admin.register(Member)
+class MemberAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+    list_display = ("name", "contact")
